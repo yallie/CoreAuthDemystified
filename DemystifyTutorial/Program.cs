07c04880f3c67a00006fa4d5eff543589c4a23ae
yallie <yallie@yandex.ru>
2017-11-28 18:00:11 +0300

    Changed the target framework to .NET 4.6.2 instead of the .NET Core. Removed the all-in-one dependency Microsoft.AspNetCore.All, which doesn't support .NET 4.6.2. Added the required Nuget packages one-by-one instead. Enabled console logging and using Kestrel host.

--- DemystifyTutorial/Program.cs
+++ DemystifyTutorial/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,12 +15,22 @@ namespace DemystifyTutorial
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
                 .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureLogging(factory =>
+                {
+                    factory.AddConsole();
+                    factory.AddFilter("Console", level => level >= LogLevel.Information);
+                })
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000);
+                })
+                ////.UseIISIntegration()
                 .Build();
+
+            host.Run();
+        }
     }
 }
