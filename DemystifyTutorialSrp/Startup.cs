ff8415e82220fa58d6301826bb9daf0a000ba0a4
yallie <yallie@yandex.ru>
2017-12-01 00:38:41 +0300

    Added the authentication middleware. Added Register Razor page.

--- DemystifyTutorialSrp/Startup.cs
+++ DemystifyTutorialSrp/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DryIoc;
 using DryIoc.MefAttributedModel;
 using DryIoc.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -25,17 +26,16 @@ namespace DemystifyTutorialSrp
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => options.LoginPath = "/account/login");
+
             services.AddMvc();
 
             return new Container()
-                // optional: to support MEF attributed services discovery
-                .WithMef()
-                // setup DI adapter
-                .WithDependencyInjectionAdapter(services,
-                    // optional: propagate exception if specified types are not resolved, and prevent fallback to default Asp resolution
-                    throwIfUnresolved: type => type.Name.EndsWith("Controller"))
-                // add registrations from CompositionRoot classs
-                .ConfigureServiceProvider<CompositionRoot>();
+                .WithMef() // optional: to support MEF attributed services discovery
+                .WithDependencyInjectionAdapter(services, // setup DI adapter
+                    throwIfUnresolved: type => type.Name.EndsWith("Controller")) // optional: propagate exception if specified types are not resolved, and prevent fallback to default Asp resolution
+                .ConfigureServiceProvider<CompositionRoot>(); // add registrations from CompositionRoot class
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +52,7 @@ namespace DemystifyTutorialSrp
             }
 
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
