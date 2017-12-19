153659ffb87dfcc0b087fe41b78e4da98a6df5f4
yallie <yallie@yandex.ru>
2017-12-19 23:36:57 +0300

    TypeScript and JQuery.

--- DemystifyTypescriptSrp/Scripts/main.ts
+++ DemystifyTypescriptSrp/Scripts/main.ts
@@ -1,5 +1,9 @@
-﻿document.getElementById("helloworld").innerText = "Hello world from the TypeScript";
+﻿// document.getElementById("helloworld").innerText = "Hello world from the TypeScript";
 
 // doesn't work in TypeScript:
 // var $ = require('jquery');
 // $("#helloworld").text("Hello world");
+
+import $ from "jquery";
+
+$("#helloworld").text("Hello world from the TypeScript and JQuery");
