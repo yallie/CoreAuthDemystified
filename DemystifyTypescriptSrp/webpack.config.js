592bccab670589692efc736ea33dfbfc9956b743
yallie <yallie@yandex.ru>
2017-12-19 23:15:08 +0300

    Added npm and webpack integration. See this tutorial: https://habrahabr.ru/post/328638/

---
+++ DemystifyTypescriptSrp/webpack.config.js
@@ -0,0 +1,25 @@
+﻿"use strict"
+{
+    // Required to form the full output paths
+    let path = require('path');
+
+    // This plugin cleans up the output bundle before bundling again
+    const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+    // Output bundle path
+    const bundleFolder = "wwwroot/bundle/";
+
+    module.exports = {
+        // Application entry point
+        entry: "./Scripts/main.js",
+
+        // Output file
+        output: {
+            filename: 'script.js',
+            path: path.resolve(__dirname, bundleFolder)
+        },
+        plugins: [
+            new CleanWebpackPlugin([bundleFolder])
+        ]
+    };
+}
